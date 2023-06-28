864e3c18dea5949d97398ee16092bbad89969f5c
Ingrid Emilia <61026310+ingrid-emilia@users.noreply.github.com>
2023-06-28 19:25:39 -0300

    Update api.php

--- laravel-docker/routes/api.php
+++ laravel-docker/routes/api.php
@@ -9,54 +9,43 @@ use App\Http\Controllers\FareController;
 
 Route::post('/quote', [FareController::class, 'quote']);
 Route::get('/metrics', [FareController::class, 'metrics']);
+
+class APIService
+{
+    protected $baseUrl;
+    protected $apiKey;
+
+    public function __construct()
+    {
+        $this->baseUrl = 'https://dev.freterapido.com/ecommerce/cotacao_v3/';
+        $this->apiKey = config('api_key');
+    }
+
+    public function getSomething()
+    {
+        $response = Http::get($this->baseUrl . '/endpoint', [
+            'api_key' => $this->apiKey,
+        ]);
+
+        return $response->json();
+    }
+}
+
 Route::post('/quote', function (Request $request) {
-    $data = $request->validate([
+    $request->validate([
         'recipient.address.zipcode' => 'required',
         'volumes' => 'required|array',
     ]);
 
-    class APIService
-    {
-        protected $baseUrl;
-        protected $apiKey;
-    
-        public function __construct()
-        {
-            $this->baseUrl = 'https://dev.freterapido.com/ecommerce/cotacao_v3/';
-            $this->apiKey = config('1d52a9b6b78cf07b08586152459a5c90'); 
-        }
-    
-        public function getSomething()
-        {
-            $response = Http::get($this->baseUrl . '/endpoint', [
-                'api_key' => $this->apiKey,
-            ]);
-    
-            return $response->json();
-        }
-    }
-    
+    $apiService = new APIService();
+    $data = $apiService->getSomething();
+
     return response()->json([
-        'carrier' => [
-            [
-                'name' => 'Frete Raápido',
-                'service' => 'Entregas',
-                'deadline' => '3',
-                'price' => 17,
-            ],
-            [
-                'name' => 'Correios',
-                'service' => 'SEDEX',
-                'deadline' => 1,
-                'price' => 20.99,
-            ],
-        ],
+        'carrier' => $data,
     ]);
 });
 
 Route::get('/metrics', function (Request $request) {
-    $lastQuotes = $request->query('last_quotes');
-
     // Consultar os retornos gravados no banco de dados e calcular as métricas
 
     return response()->json([
